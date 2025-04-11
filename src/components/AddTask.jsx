5b9cc2826df40449b6d1623d162395f81e2d098d
Rubenslourenco <alvesrubens926@gmail.com>
2025-04-11 20:05:53 -0300

    feat: add componentes Task e AddTaks

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,5 @@
+const AddTask = () => {
+  return <h1>Add Taks</h1>;
+};
+
+export default AddTask;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,6 @@
+const Tasks = () => {
+  return <h1>Taks</h1>;
+};
+
+export default Tasks;
+<h1>Taks</h1>;
