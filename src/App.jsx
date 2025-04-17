5652121bb00b2c000836301a1d6ea5476ebf15b0
Rubenslourenco <alvesrubens926@gmail.com>
2025-04-17 17:44:52 -0300

    feat: add metodo para adicionar tarefa no pelo input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 import { useState } from "react";
 
@@ -43,13 +44,22 @@ const App = () => {
     setTasks(newtasks);
   }
 
+  function OnAddTaskSubmit(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center  p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        {/* <AddTask /> */}
+        <AddTask onTaskClick={OnAddTaskSubmit} />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,31 @@
-const AddTask = () => {
-  return <h1>Add Taks</h1>;
-};
+import { useState } from "react";
 
+const AddTask = ({ OnAddTaskSubmit }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite sua tarefa"
+        className="border  border-slate-400 outline-slate-400  px-4 py-2 rounded-md"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa tarefa"
+        className="border  border-slate-400 outline-slate-400  px-4 py-2 rounded-md"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button
+        className="bg-slate-500 text-white px-4 py-2 rounded-md "
+        onClick={() => OnAddTaskSubmit(title, description)}
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+};
 export default AddTask;
